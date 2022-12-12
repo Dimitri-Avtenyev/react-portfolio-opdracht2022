d7fb046bae24ecf7b4c2f5f00bc320dcd52f0b10
Avtenyev Dimitri (s136657) <dimitri.avtenyev@student.ap.be>
2022-12-12 23:08:06 +0100

    contact fix css

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -22,44 +22,44 @@ const Contact = () => {
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         //sendTestMail();
-        setConfirmationText(`Thanks for your message, ${name}!`);
+        setConfirmationText(`Thank you for your message, ${name}!`);
         setFormSubmitted(true);
     };
     return (
         <div className={styles.ContactContainer}>
             <div className={styles.form}>
-            <div>
-                <h1>Let's get in touch!</h1>
-            </div>
-            <Form className={styles.form} onSubmit={handleSubmit}>
-                <FloatingLabel controlId="floatingInput" label="Email address">
-                    <Form.Control type="email" placeholder="name@example.com" 
-                    onChange={(e) => {setEmail(e.target.value)}} value={email}
-                    />
-                </FloatingLabel>
-                <FloatingLabel controlId="floatingInput" label="Name">
-                    <Form.Control type="text" placeholder="Name" 
-                    onChange={(e) => {setName(e.target.value)}} value={name}
-                    />
-                </FloatingLabel>
-                <FloatingLabel controlId="floatingTextArea" label="Leave a message!">
-                    <Form.Control as="textarea" placeholder="Leave a comment here" 
-                    onChange={(e) => {setMessage(e.target.value)}} value={message}
-                    />
-                </FloatingLabel>
-                <Button variant="primary" type="submit" disabled={checkInputEmpty()}>
-                    Submit
-                </Button>
-            </Form>
-            <div>
-                <p>{formSubmitted ? confirmationText : ""}</p>
+                <div>
+                    <h1>Let's get in touch!</h1>
+                </div>
+                <Form className={styles.form} onSubmit={handleSubmit}>
+                    <FloatingLabel controlId="floatingInput" label="Email address">
+                        <Form.Control type="email" placeholder="name@example.com" 
+                        onChange={(e) => {setEmail(e.target.value)}} value={email}
+                        />
+                    </FloatingLabel>
+                    <FloatingLabel controlId="floatingInput" label="Name">
+                        <Form.Control type="text" placeholder="Name" 
+                        onChange={(e) => {setName(e.target.value)}} value={name}
+                        />
+                    </FloatingLabel>
+                    <FloatingLabel controlId="floatingTextArea" label="Leave a message!">
+                        <Form.Control as="textarea" placeholder="Leave a comment here" 
+                        onChange={(e) => {setMessage(e.target.value)}} value={message}
+                        />
+                    </FloatingLabel>
+                    <Button variant="primary" type="submit" disabled={checkInputEmpty()}>
+                        Submit
+                    </Button>
+                </Form>
+                <div>
+                    <p>{formSubmitted ? confirmationText : ""}</p>
+                </div>
             </div>
             <div className={styles.socials}>
                 <ul>
                     <li><a href="https://github.com/Dimitri-Avtenyev" target="blank"><img src="./githubLogo.png" width="30px" alt="github-logo"/></a></li>
                     <li><a href="https://www.linkedin.com/in/dimitri-avtenyev/v" target="blank"><img src="./linkedinLogo.png" width="30px" alt="linkedin-logo"/></a></li>
                 </ul>
-            </div>
             </div>
         </div>
     )
